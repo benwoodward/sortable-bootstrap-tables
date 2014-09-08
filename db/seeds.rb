3638e953b260a24d9c75464d18ee1194e672d021
Ben Woodward <b@benw.me>
2014-09-08 10:19:23 +1000

    add db/seeds.rb for 3 Things

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Thing.create(title: 'A Roman Dodecahedra',
+             description: 'These fist-sized
+               bronze Roman artifacts found in France, Switzerland and Germany
+               pose a fascinating problem for archaeologists: they just don’t
+               have a clear purpose, but many are covered in symbols, some
+               undecipherable and others relating to the Zodiac. But for all the
+               speculation on their use, including that they may have been
+               surveying instruments, some experts believe the Roman dodecahedra
+               were merely decorative candlesticks.')
+
+Thing.create(title: 'A Phaistos Disc',
+             description: 'There’s very little that we actually know for sure
+               about the Phaistos Disc. It’s made of clay – check. It dates back to the second
+               millenium B.C.E. – maybe. But its origin, meaning and purpose remain shrouded
+               in mystery. Discovered in Crete, the disc is features i241 impressions of 45
+               distinct symbols, some of which are easily identifiable as people, tools,
+               plants and animals. But because nothing else like it from the same time period
+               has ever been found, archaelogists haven’t been able to provide a meaningful
+               analysis of its content.')
+
+Thing.create(title: 'The Voynich Manuscript',
+             description: 'Is the Voynich Manuscript evidence of a forgotten
+               civilization, or merely an elaborate hoax? This handwritten book full of text
+               that the world’s top cryptographers and codebreakers have never been able to
+               decipher dates to the 15th century and was discovered in 1912 by book dealer
+               Wilfrid M. Voynich. If it is a hoax, it’s incredibly convincing, given how
+               fluidly the text was written and the fact that statistical analysis has
+               revealed patterns similar to those found in natural languages')
