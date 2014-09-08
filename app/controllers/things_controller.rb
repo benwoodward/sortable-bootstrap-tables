58785d607cdfbc87a852838fa28f5b865edd1d20
Ben Woodward <b@benw.me>
2014-09-08 10:30:30 +1000

    remove unnecessary controller actions

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -12,55 +12,6 @@ class ThingsController < ApplicationController
   def show
   end
 
-  # GET /things/new
-  def new
-    @thing = Thing.new
-  end
-
-  # GET /things/1/edit
-  def edit
-  end
-
-  # POST /things
-  # POST /things.json
-  def create
-    @thing = Thing.new(thing_params)
-
-    respond_to do |format|
-      if @thing.save
-        format.html { redirect_to @thing, notice: 'Thing was successfully created.' }
-        format.json { render :show, status: :created, location: @thing }
-      else
-        format.html { render :new }
-        format.json { render json: @thing.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /things/1
-  # PATCH/PUT /things/1.json
-  def update
-    respond_to do |format|
-      if @thing.update(thing_params)
-        format.html { redirect_to @thing, notice: 'Thing was successfully updated.' }
-        format.json { render :show, status: :ok, location: @thing }
-      else
-        format.html { render :edit }
-        format.json { render json: @thing.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /things/1
-  # DELETE /things/1.json
-  def destroy
-    @thing.destroy
-    respond_to do |format|
-      format.html { redirect_to things_url, notice: 'Thing was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_thing
